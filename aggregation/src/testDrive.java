b7dfe23b916988e29566cfd4a733bbd9db124c35
nattavot014 64/45 <120540313+aom081@users.noreply.github.com>
2024-07-03 12:16:25 +0700

    Add files via upload

---
+++ aggregation/src/testDrive.java
@@ -0,0 +1,9 @@
+
+
+public class testDrive {
+	 public static void main(String[] args) {
+	        Address address = new Address("123 Main St", "Springfield", "IL", "62701");
+	        Person person = new Person("John Doe", 30, address);
+	        System.out.println(person);
+	    }
+}
