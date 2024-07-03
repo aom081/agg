891ef597b2724ba606efa311873ec43bfcec46e1
nattavot014 64/45 <120540313+aom081@users.noreply.github.com>
2024-07-03 12:32:01 +0700

    Add files via upload

---
+++ agg/Process.php
@@ -0,0 +1,17 @@
+<?php
+include "./model/Addess.php";
+include "./model/Person.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $street = $_POST['street'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $postalCode = $_POST['postalCode'];
+
+    $address = new Address($street, $city, $state, $postalCode);
+    $person = new Person($name, $age, $address);
+
+    echo $person;
+}
---
+++ agg/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+// สร้าง instance ของ Address
+$address = new Address('123 Main St', 'Springfield', 'IL', '62701');
+
+// สร้าง instance ของ Person โดยใช้ address ที่สร้างไว้
+$person = new Person('John Doe', 30, $address);
+
+echo $person;
